bd6128b943b0dfdb264f529c821ee74ba8e79eef
Zverobob <valera9999z@gmail.com>
2022-07-21 00:07:04 +0300

    Added new db + changed the script

--- scr/buttons.js
+++ scr/buttons.js
@@ -1,16 +1,20 @@
 'use strict'
-
+let fileName = "";
 const locale = {
+
     data:[],
 
     buttons: [],
 
     //Extracting JSON data
     getData: function() {
-        fetch('scr/locale.json')
+        fileName = location.pathname.split("/").slice(-1);
+        fileName = 'scr/' + fileName[0].replace('html', 'json');
+        fetch(fileName)
         .then(data => data.json())
         .then(json => {
             this.data = json;
+            console.log(fileName);
             this.createLocaleButtons();
             this.buttons = document.querySelectorAll('.lang-btn'); 
             this.addHandlers();
@@ -28,7 +32,9 @@ const locale = {
             locBtn.classList.add('lang-btn');
             if(key == "English"){
                 locBtn.setAttribute("data-sel", 1);
-                locBtn.classList.add('active');
+                if(fileName == "scr/guidlines.json")
+                    locBtn.classList.add('red');
+                else locBtn.classList.add('active');
             }
             else locBtn.setAttribute("data-sel", 0);
             locBtn.setAttribute("data-lang", key);
@@ -41,15 +47,19 @@ const locale = {
         Section.innerHTML = "";
         locale.data[lang].forEach(element => {
             const title = document.createElement('dt');
-            title.textContent = element["title"];
-            const text = document.createElement('dd');
-            text.textContent = element["text"];
             if( lang == "Russian"){
                 title.classList.add('RUS');
-                text.classList.add('RUS');
             }
+            title.textContent = element["title"];
             Section.appendChild(title);
-            Section.appendChild(text);
+            for (const key in element["text"]){
+                const text = document.createElement('dd');  
+                if( lang == "Russian"){
+                    text.classList.add('RUS');
+                }
+                text.textContent = element["text"][key];
+                Section.appendChild(text);
+            }
         });
     },
 
@@ -58,10 +68,13 @@ const locale = {
         const buttons = document.querySelectorAll('.lang-btn'); 
         if( btn.target.dataset.sel == 0){
             btn.target.classList.add('active');
+            if(fileName == "scr/guidlines.json")
+                btn.target.classList.add('red');
             console.log("Switching to", btn.target.dataset.lang);
             buttons.forEach(element => {
                 if (element.dataset.sel == 1){
                     element.classList.remove('active');
+                    element.classList.remove('red');
                     element.dataset.sel = 0;
                 }
             }); 
